f3dd31fc5954fabc2b9f1b1d8c8f99fada060035
nilesh <akhajare96@gmail.com>
2020-10-07 10:59:56 +0530

    modify main sop

--- Demo.java
+++ Demo.java
@@ -1,6 +1,7 @@
 public class Demo{
    public static void main(String [] args ){
-    System.out.println("Commit to repository.....")
+    System.out.println("Commit to repository.....mm");
+    System.out.println("nilz");
   
 }
   
